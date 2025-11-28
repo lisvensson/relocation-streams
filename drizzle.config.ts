cbb5397801b628f3a2cac6d103ad04e54d15ae0c
Li Svensson <lisvensson@msn.com>
2025-11-28 15:54:00 +0100

    changed from PUBLIC_DATABASE_URL to DATABASE_URL in drizzle.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.PUBLIC_DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
   throw Error('DATABASE_URL is required')
 }
 
 export default defineConfig({
-  dbCredentials: { url: process.env.PUBLIC_DATABASE_URL },
+  dbCredentials: { url: process.env.DATABASE_URL },
   out: './drizzle',
   schema: './app/shared/database/schema.ts',
   dialect: 'postgresql',
