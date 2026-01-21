8160757c01eed72ba43e79671d9b85184a660ec7
Li Svensson <lisvensson@msn.com>
2026-01-21 14:02:50 +0100

    Added new chartModels

---
+++ app/models/chartModels.ts
@@ -0,0 +1,98 @@
+export type Measure = 'inflow' | 'outflow'
+
+type Category =
+  | 'employeeRange'
+  | 'industryCluster'
+  | 'companyType'
+  | 'postalAreaCode'
+  | 'postalAreaName'
+  | 'municipality'
+  | 'county'
+
+export type ChartUiSettings = {
+  containerSize: 'small' | 'medium' | 'large' // ??
+  legendPlacement: 'hidden' | 'top' | 'bottom' | 'left' | 'right'
+  tablePlacement: 'hidden' | 'top' | 'bottom' | 'left' | 'right'
+}
+
+export type TemporalChartConfig = {
+  type: 'temporal'
+  title: string
+  filters?: Filter[]
+  uiSettings?: ChartUiSettings
+  measure: Measure
+}
+
+export type CategoryChartConfig = {
+  type: 'category'
+  title: string
+  filters?: Filter[]
+  uiSettings?: ChartUiSettings
+  measure: Measure
+  category: Category
+  maxNumberOfCategories: number
+  combineRemainingCategories: boolean
+  chartType: 'pie' | 'bar'
+}
+
+export type TemporalCategoryChartConfig = {
+  type: 'temporal+category'
+  title: string
+  filters?: Filter[]
+  uiSettings?: ChartUiSettings
+  measure: Measure
+  category: Category
+  maxNumberOfCategories: number
+  combineRemainingCategories: boolean
+  measureCalculation: 'volume' | 'percent'
+}
+
+export type NetFlowChartConfig = {
+  type: 'netflow'
+  title: string
+  filters?: Filter[]
+}
+
+export type ChartConfig =
+  | TemporalChartConfig
+  | CategoryChartConfig
+  | TemporalCategoryChartConfig
+  | NetFlowChartConfig
+
+export type ChartModel = {
+  title: string
+  type: ChartType
+  measure: Measure // ??
+  dimension: Dimension
+  series: string[]
+  data: ChartDataPoint[]
+}
+
+export type TemporalDimension = 'year' | 'month' | 'week'
+export type Dimension = TemporalDimension | Category
+export type ChartType = 'column' | 'bar' | 'pie' | 'line'
+export type ChartDataPoint = {
+  dimension: string
+  [key: string]: number | string | null | undefined
+}
+
+export type Filter = InArrayFilter /*| EqualsFilter |  | BetweenFilter*/
+
+export type InArrayFilter = {
+  key: string
+  operator: 'in'
+  value: string[] | number[]
+}
+/*
+export type EqualsFilter = {
+  key: string;
+  operator: "equals";
+  value: string;
+};
+
+export type BetweenFilter = {
+  key: string;
+  operator: "between";
+  value: [number, number];
+};
+*/
