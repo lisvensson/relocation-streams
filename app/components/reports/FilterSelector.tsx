ce7ce36239ce804177a777c16b2928d04b446115
Li Svensson <lisvensson@msn.com>
2026-03-20 09:37:46 +0100

    moved files and small updates to filterSelector

--- app/components/reports/FilterSelector.tsx
+++ app/components/reports/FilterSelector.tsx
@@ -10,19 +10,22 @@ export function FilterSelector<T extends string | number>({
   searchParams,
 }: FilterSelectorProps<T>) {
   return (
-    <>
+    <div className="grid grid-cols-2 gap-2">
       {items.map((item) => (
-        <label key={item} className="block mb-2">
+        <label
+          key={item}
+          className="flex items-center gap-2 text-sm cursor-pointer"
+        >
           <input
             type="checkbox"
             name={name}
             value={item}
             defaultChecked={searchParams.has(name, String(item))}
-            className="mr-2"
+            className="h-4 w-4"
           />
-          {item}
+          <span>{item}</span>
         </label>
       ))}
-    </>
+    </div>
   )
 }
